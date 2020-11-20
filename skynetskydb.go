6c69defc5f0ff2dc238f2b72f89f6cbd5154a46d
AutisticVegan <vehman@gmail.com>
2020-11-20 04:36:33 -0800

    shit is still broken - fix later lulz

--- skynetskydb.go
+++ skynetskydb.go
@@ -16,6 +16,7 @@ import (
 	"net/http"
 	"encoding/json"
 	"io/ioutil"
+	"time"
 
 	"golang.org/x/crypto/blake2b"
 	"golang.org/x/crypto/pbkdf2"
@@ -41,6 +42,9 @@ func GetBytesFromRegistry(publicKey string, dataKey string, portal string) ([]by
 	}
 	// download the data
 	skynetClient := skynet.NewCustom(portal, skynet.Options{})
+	if len(registryEntry.data) == 0 {
+		return []byte{}, 0
+	}
 	// make sure this data is the skylink
 	buff, err := skynetClient.Download(registryEntry.data, skynet.DefaultDownloadOptions)
 	if err != nil {
@@ -70,10 +74,14 @@ func UpdateRegistry(privKey []byte, pubKey []byte, dataKey string, skyLink strin
 	}
     current.data = skyLink      
     //Set new version
-    return setEntry(privKey, pubKey, current, portal);
+    return setEntry(pubKey, privKey, current, portal);
 }
 
-func SetBytesToRegistry(privateKey string, publicKey string, dataKey string, revision int, input []byte, portal string) error {
+func trimSkylink(sklink string) string {
+	return sklink[6:]
+}
+
+func SetBytesToRegistry(privateKey string, publicKey string, dataKey string, input []byte, portal string) error {
 	skynetClient := skynet.NewCustom(portal, skynet.Options{})
 	uploadData := make(skynet.UploadData)
 	uploadData[dataKey] = bytes.NewReader(input)
@@ -83,7 +91,8 @@ func SetBytesToRegistry(privateKey string, publicKey string, dataKey string, rev
 		fmt.Println(err)
 		return err
 	}
-
+	// trim skylink
+	skylink = trimSkylink(skylink)
 	return UpdateRegistry([]byte(privateKey), []byte(publicKey), dataKey, skylink, portal)
 }
 
@@ -111,17 +120,17 @@ type registryResponse struct {
 //revision
 //signature
 
-type publicKeyStruct struct {
-	algorithm string // is ed25519
-	key []byte
+type PublicKeyStruct struct {
+	Algorithm string // is ed25519
+	Key []byte
 }
 
-type entryToSet struct {
-	data string
-	datakey string
-	publicKey publicKeyStruct
-	revision int
-	signature []byte
+type EntryToSet struct {
+	Data string
+	Datakey string
+	PublicKey PublicKeyStruct
+	Revision int
+	Signature []byte
 }
 
 func decodeHex(input []byte) []byte {
@@ -136,9 +145,20 @@ func getJSONResponseAndTransformToSignedRegistryEntry(
 	datakey []byte) RegistryEntry {
 
 	registryResponse := new(registryResponse)
-	json.NewDecoder(resp.Body).Decode(registryResponse)
+	fmt.Println(resp.Body)
+	err := json.NewDecoder(resp.Body).Decode(registryResponse)
+
+	if err != nil {
+		return RegistryEntry{
+			datakey: string(datakey),
+			data: "",
+			revision: 0,
+		}
+	}
+
 	resp.Body.Close()
 
+	/*
 	registryEntryToBeHashed := RegistryEntry{
 		datakey: string(datakey),
 		data: string(registryResponse.data),
@@ -151,6 +171,7 @@ func getJSONResponseAndTransformToSignedRegistryEntry(
 	if !verified {
 		fmt.Println("ed25519 verification was false")
 	}
+	*/
 
 	returnRegistryEntry := RegistryEntry {
 		data: string(decodeHex(registryResponse.data)),
@@ -163,21 +184,22 @@ func getJSONResponseAndTransformToSignedRegistryEntry(
 func getEntry(publicKey ed25519.PublicKey, dataKey []byte, portal string) (RegistryEntry, error) {
 	hexDataKey := hex.EncodeToString(hashAll([][]byte{dataKey}))
 	hexPublicKey := hex.EncodeToString(publicKey)
-    client := &http.Client{}
+    client := &http.Client{Timeout: 30 * time.Second,}
 	req, _ := http.NewRequest("GET", portal + defaultPath, nil)
 	q := req.URL.Query()
 	// should this be "ed25519:" and just the hexdatakey
-    q.Add("publickey", hexPublicKey)
+    q.Add("publickey", "ed25519:" + hexPublicKey)
     q.Add("datakey", hexDataKey)
 	// TODO(autisticvegan): What are you going to do about timeouts here? etc (code hygiene needed)
 	// req.Header.Add("Accept", "application/json")
 	req.URL.RawQuery = q.Encode()
+	fmt.Println(req.URL)
 	resp, err := client.Do(req)
 	if err != nil {
 		r := RegistryEntry {
 			data: "",
-			datakey: "",
-			revision: -69,
+			datakey: hexDataKey,
+			revision: 0,
 		}
 		return r, err
 	}
@@ -194,17 +216,17 @@ func setEntry(publicKey ed25519.PublicKey, privateKey ed25519.PrivateKey, entry 
 	// revision is int
 	// signature is bytes
 
-	publicKeyStruct := publicKeyStruct {
-		algorithm: "ed25519",
-		key: publicKey,
+	publicKeyStruct := PublicKeyStruct {
+		Algorithm: "ed25519",
+		Key: publicKey,
 	}
 
-	actualEntryToSet := entryToSet {
-		data: entry.data,
-		datakey: hex.EncodeToString([]byte(entry.datakey)),
-		publicKey: publicKeyStruct,
-		revision: entry.revision,
-		signature: sig,
+	actualEntryToSet := EntryToSet {
+		Data: entry.data,
+		Datakey: entry.datakey,
+		PublicKey: publicKeyStruct,
+		Revision: entry.revision,
+		Signature: sig,
 	}
 
 	marshaledJSON, err := json.Marshal(actualEntryToSet)
@@ -215,6 +237,7 @@ func setEntry(publicKey ed25519.PublicKey, privateKey ed25519.PrivateKey, entry 
 	fmt.Println("POSTing for setEntry()...")
 	client := &http.Client{}
 	req, _ := http.NewRequest("POST", portal + defaultPath, bytes.NewBuffer(marshaledJSON))
+	req.Header.Set("Content-type", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
